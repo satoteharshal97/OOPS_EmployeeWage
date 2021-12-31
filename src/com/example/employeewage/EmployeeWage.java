13db0af25c0f55892203f51255117e37b8a9815f
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-31 15:00:37 +0530

    Addded - Check present or absent

---
+++ src/com/example/employeewage/EmployeeWage.java
@@ -0,0 +1,20 @@
+package com.example.employeewage;
+
+public class EmployeeWage {
+
+    public static final int IS_FULL_TIME = 1;
+
+    public static void main(String[] args) {
+        checkAbsentOrPresent();
+    }
+
+    static void checkAbsentOrPresent(){
+        double empChecker = (int)Math.floor(Math.random() * 10) % 2;
+        if(empChecker == IS_FULL_TIME) {
+            System.out.println("Employee is Present");
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
+    }
+}
