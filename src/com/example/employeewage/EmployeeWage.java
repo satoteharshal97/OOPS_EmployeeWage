544822c12f541c8cfd833d530a7f72616fb5c1b6
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-31 15:37:24 +0530

    Added - Get monthly wage

--- src/com/example/employeewage/EmployeeWage.java
+++ src/com/example/employeewage/EmployeeWage.java
@@ -4,14 +4,16 @@ public class EmployeeWage {
     public static final int EMP_WAGE_PER_HOUR = 20;
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
+    public static final int NUM_OF_WORKING_DAYS = 20;
 
     public static void main(String[] args) {
-        getFullOrPartTimeWage();
+        getMonthlyWage();
     }
-    static void getFullOrPartTimeWage(){
-        int empHours = 0;
-        int empWage = 0;
-        int empChecker = (int)Math.floor(Math.random() * 10) % 3;
+    static void getMonthlyWage(){
+        int empHours, empWage, totalEmpWage = 0;
+        for(int day = 0; day < NUM_OF_WORKING_DAYS;  day++){
+            int empChecker = (int)Math.floor(Math.random() * 10) % 3;
+
             switch(empChecker){
                 case IS_FULL_TIME:
                     empHours = 8;
@@ -23,8 +25,11 @@ public class EmployeeWage {
                     empHours = 0;
             }
             empWage = empHours * EMP_WAGE_PER_HOUR;
+            totalEmpWage = totalEmpWage + empWage;
             System.out.println("Employee Wage: " + empWage);
         }
+        System.out.println("Total Employee Wage: " + totalEmpWage);
+    }
 }
 
 
