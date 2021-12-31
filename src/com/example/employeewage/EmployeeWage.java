b0dc55ccf486686995608651c0ee77b476d47d4e
satoteharshal97 <37376689+satoteharshal97@users.noreply.github.com>
2021-12-31 15:29:28 +0530

    Addded - Get full or part time wage

--- src/com/example/employeewage/EmployeeWage.java
+++ src/com/example/employeewage/EmployeeWage.java
@@ -3,17 +3,21 @@ package com.example.employeewage;
 public class EmployeeWage {
     public static final int EMP_WAGE_PER_HOUR = 20;
     public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
 
     public static void main(String[] args) {
-        dailyWage();
+        getFullOrPartTimeWage();
     }
-    static void dailyWage(){
+    static void getFullOrPartTimeWage(){
         int empHours = 0;
         int empWage = 0;
-        double empChecker = (int)Math.floor(Math.random() * 10) % 2;
+        double empChecker = (int)Math.floor(Math.random() * 10) % 3;
 
         if(empChecker == IS_FULL_TIME) {
             empHours = 8;
+        }
+        else if(empChecker == IS_PART_TIME){
+            empHours = 4;
         }
         else {
             empHours = 0;
@@ -22,3 +26,5 @@ public class EmployeeWage {
         System.out.println("Employee Wage: " + empWage);
     }
 }
+
+
